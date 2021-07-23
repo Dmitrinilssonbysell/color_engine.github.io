83a358b85b13ed4545393b2544b4effebae019f9
nilssonbysell <nilssonbysell@hotmail.com>
2021-07-23 17:13:55 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function ColorEngine(color, selector) {
+    const doc = document.querySelectorAll(selector)
+
+    if (color === "black") {
+        return doc.style.background = "rgb(45,45,45)"
+    }
+    else if (color === blue) {
+        return doc.style.background = "rgb(20,120,255)"
+    }
+}
+
+module.exports.ColorEngine = ColorEngine
